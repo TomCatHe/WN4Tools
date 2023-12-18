9e02a59d14cfcd3800fa1b661285f37f26fa19cc
TomCatHe <heckerchong@gmail.com>
2023-12-19 00:14:04 +0800

    6

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
-    <div style="margin-top: 20px">
-        <table border width="600" cellpadding="0" :cellspacing="0">
-            <thead>
-                <tr>
-                    <th>文件</th>
-                    <th>状态</th>
-                    <th>操作</th>
-                </tr>
-            </thead>
-            <tbody v-for="item in dataList">
-                <tr>
-                    <td>{{ item.name }}</td>
-                    <td>{{ item.status }}</td>
-                    <td><button>Del</button></td>
-                </tr>
-            </tbody>
-        </table>
+    <div id="container">
+        <div id="filelist">文件列表</div>
+        <div>
+            <table width="600">
+                <thead>
+                    <tr>
+                        <th>文件</th>
+                        <th>状态</th>
+                        <th>操作</th>
+                    </tr>
+                </thead>
+                <tbody v-for="item in dataList">
+                    <tr>
+                        <td>{{ item.name }}</td>
+                        <td>{{ item.status }}</td>
+                        <td><button>Del</button></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
@@ -43,8 +46,46 @@ let dataList = reactive<FileData[]>([
         name: "c.sql",
         status: "read",
         operation: "Del",
-    }
+    },
 ]);
 </script>
 
-<style scoped></style>
+<style scoped>
+* {
+    padding: 0%;
+    margin: 0%;
+}
+
+#container {
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+    /* background-color: #ccc; */
+}
+
+#filelist {
+    height:200px;
+    width:100px;
+    float: left;
+}
+
+table {
+    border: 1px solid #000;
+    border-collapse: collapse;
+    width:"600px";
+}
+
+th,
+td {
+    border: 1px solid #000;
+    padding: 5px;
+}
+
+th {
+    background-color: #ccc;
+}
+
+td {
+    background-color: #eee;
+}
+</style>
