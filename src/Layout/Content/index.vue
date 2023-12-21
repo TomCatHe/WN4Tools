438c1508a82efcc4b345c8ef9a0586123a3ad6c5
TomCatHe <heckerchong@gmail.com>
2023-12-21 18:29:39 +0800

    8

--- src/Layout/Content/index.vue
+++ src/Layout/Content/index.vue
@@ -1,6 +1,22 @@
 <template>
     <div class="wn-content">
-        <div class="wn-content__items" v-for="item in 100">{{ item }}</div>
+        <!-- <div v-for="item in 100">{{ item }}</div> -->
+        <table>
+            <thead>
+                <tr>
+                    <th>文件名称</th>
+                    <th>状态</th>
+                    <th>信息</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in 100">
+                    <td>{{ item }}</td>
+                    <td>18</td>
+                    <td>男</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -10,11 +26,27 @@
 @include b(content) {
     flex: 1;
     overflow: auto;
-    @include e(items) {
-        padding: 10px;
-        margin: 10px;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-    }
+    // @include e(items) {
+    //     padding: 1%;
+    //     margin: 1%;
+    //     border: 1px solid #ccc;
+    //     border-radius: 4px;
+    //     border-collapse: collapse;
+    //     // 表格列自适应宽度
+    //     table-layout: fixed;
+    //     width: 98%;
+    // }
+}
+
+table,
+td {
+    padding: 1%;
+    margin: 1%;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    border-collapse: collapse;
+    // 表格列自适应宽度
+    table-layout: fixed;
+    width: 98%;
 }
 </style>
